12b519e54f311859c7a0d0b32dfc2171a2f82914
Sipha363 <siphamandla@neolytics.io>
2024-09-22 22:48:03 +0200

    update ui select

--- src/components/Contact/MultiSelect.tsx
+++ src/components/Contact/MultiSelect.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 import Select from "react-select";
 import { ClearIndicator } from "./ClearIndicator";
-//https://codesandbox.io/s/react-select-multiselect-with-material-ui-tooltip-for-clear-indicator-ky133?file=/src/App.js:631-682
 
-const Multiselect = ({ selected, setSelected, options , labelname, Iwidth, placeholder}) => {
-  const onChange = selectedOptions => {
+const Multiselect = ({ selected, setSelected, options, labelname, Iwidth, placeholder }) => {
+  const onChange = (selectedOptions) => {
     setSelected(selectedOptions);
   };
+
+  // Custom styles for react-select components
   const customStyles = {
-    indicatorSeparator: () => {}
+    indicatorSeparator: () => ({
+      display: 'none',
+    }),
   };
+
   return (
-    <div style={{ 'width':Iwidth }}>
+    <div style={{ width: Iwidth }}>
+      {/* Render the label above the Select component */}
+      {labelname && <label className="text-sm font-medium text-dark">{labelname}</label>}
       <Select
-      isMulti
-      label={labelname}
-      isClearable={true}
-      components={{ ClearIndicator }}
-      defaultValue={selected}
-      value={selected}
-      styles={customStyles}
-      onChange={onChange}
-      options={options}
-      isSearchable={true}
-      placeholder={placeholder}
-    />
+        isMulti
+        isClearable={true}
+        components={{ ClearIndicator }}
+        defaultValue={selected}
+        value={selected}
+        styles={customStyles}
+        onChange={onChange}
+        options={options}
+        isSearchable={true}
+        placeholder={placeholder}
+      />
     </div>
-    
   );
 };
 
